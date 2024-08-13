abf2024fedb47e19f4bba13ee9f51cd478083465
Javi <javi@whykeyway.com>
2024-08-13 14:19:34 -0300

    feat: lazy loading

--- main.py
+++ main.py
@@ -1,53 +1,28 @@
-from typing import Optional
-
-from fastapi import FastAPI, Query
-from botasaurus_api import Api
-
-app = FastAPI()
-_api_instance = None
+from botasaurus_server.server import Server
+from botasaurus_server import task_routes
+from botasaurus_server.task_routes import OK_MESSAGE
+from bottle import run, get
+import backend.scrapers
 
 
-def get_api_instance():
-    global _api_instance
-    if _api_instance is None:
-        _api_instance = Api(create_response_files=False)
-    return _api_instance
+@get("/health")
+def get_api():
+    return OK_MESSAGE
 
 
-@app.get("/health")
-def get_properties():
-    return {"status": "ok"}
+if __name__ == "__main__":
+    print("Running backend...")
+    task_routes.executor.load()
+    task_routes.executor.start()
 
+    if not Server.get_scrapers_names():
+        raise RuntimeError("No scrapers found. Please add a scraper using Server.add_scraper.")
 
-@app.get("/single_task")
-def create_task(
-    query: str = Query(description="query to scrape"),
-    max_reviews: Optional[int] = Query(default=100, description="max qty of reviews"),
-):
-    data = {
-        'queries': [query],
-        'country': None,
-        'business_type': '',
-        'max_cities': None,
-        'randomize_cities': True,
-        'api_key': '',
-        'enable_reviews_extraction': False,
-        'max_reviews': max_reviews,
-        'reviews_sort': 'newest',
-        'lang': 'en',
-        'max_results': None,
-        'coordinates': '',
-        'zoom_level': 14,
-    }
+    host = 'localhost'
+    debug = False
+    run(
+        host=host,
+        port=8080,
+        debug=debug
+    )
 
-    # Lazy load the Api instance
-    api = get_api_instance()
-    task = api.create_sync_task(data=data, scraper_name='google_maps_scraper')
-
-    return task
-
-
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8080)
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python run.py backend --force & python main.py
+python main.py
